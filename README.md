0ec32fba1392d214d2e7ad386c09708936030618
Carl Harris <elgoogemail2007@gmail.com>
2018-06-12 02:10:14 -0400

    added config, more docs

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # emailworker
 
 Python 3 email sending worker using pika and RabbitMQ. Minimal email worker for my own needs.
+
+If it exists, configuration is pulled from `./instance/config.py`
--- emailworker/__init__.py
+++ emailworker/__init__.py
@@ -4,127 +4,159 @@ import json
 import logging
 import logging.config
 import smtplib
-from configparser import ConfigParser
+import types
 from email.message import EmailMessage
+from pathlib import Path
 
 import pika
 
-# NOTE: disabling interpolation because of logging options
-config = ConfigParser(interpolation=None)
+serializer = json.dumps
+deserializer = json.loads
 
 def bind_and_call(func, data):
+    """
+    Bind the keys of `data` to the arguments of `func` and call `func` with
+    those arguments.
+
+    :param func: A callable.
+    :type func: callable
+
+    :param data: A mapping object.
+    :type data: dict
+    """
     bound = inspect.signature(func).bind(**data)
     return func(*bound.args, **bound.kwargs)
 
-def setup():
+def sendemail(host, fromaddr, toaddrs, body, subject=None):
     """
-    Setup configuration.
-    """
-    files = config.read(['default.cfg', 'instance/config.cfg'])
-    if 'loggers' in config:
-        logging.config.fileConfig(config)
-    logger = logging.getLogger('emailworker')
-    if files:
-        logger.info('successfully read %s', files)
-    else:
-        logger.info('using config defaults')
-    for section in config.sections():
-        for key, value in config[section].items():
-            logger.info('config: [%s] %s = %r', section, key, value)
+    Send an email.
 
-def sendemail(fromaddr, toaddrs, body, subject=None):
-    """
-    Send email using configured smtp_host.
+    :param host: SMTP host.
+    :type host: str
+
+    :param fromaddr: The from address.
+    :type fromaddr: str
+
+    :param toaddrs: The to address(es). Comma-separated if more than one.
+    :type toaddrs: str
+
+    :param body: The body of the email.
+    :type body: str
+
+    :param subject: The subject of the email. An empty string is used if None.
+                    Default: None.
+    :type subject: str or None
     """
     emailmessage = EmailMessage()
     emailmessage['From'] = fromaddr
     emailmessage['To'] = toaddrs
-    emailmessage['Subject'] = subject or ''
+    emailmessage['Subject'] = subject if subject is not None else ''
     emailmessage.set_content(body)
-    if not config['emailworker'].getboolean('dryrun'):
-        server = smtplib.SMTP(config['emailworker']['smtp_host'])
-        server.send_message(emailmessage)
-        server.quit()
-    logger = logging.getLogger('emailworker.send')
-    logger.info('fromaddr = %r, toaddrs = %r, body = %r, subject = %r',
-                fromaddr, toaddrs, body, subject)
+    server = smtplib.SMTP(host)
+    server.send_message(emailmessage)
+    server.quit()
 
-def publish(fromaddr, toaddrs, body, subject=None):
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=config['emailworker']['rabbitmq_host'])
-    )
-    channel = connection.channel()
-    channel.basic_publish(
-        exchange = '',
-        routing_key = config['emailworker']['queue_name'],
-        body = json.dumps(
-            dict(
-                fromaddr = fromaddr,
-                toaddrs = toaddrs,
-                body = body,
-                subject = subject,
-            )
+class Config(dict):
+    """
+    A clone of the Flask configuration object.
+    """
+    def from_pyfile(self, filename):
+        """
+        Pull UPPERCASE attributes from `filename` and store in self.
+
+        :param filename: Path to `.py` file.
+        :type filename: str or pathlib.Path
+        """
+        d = types.ModuleType('config')
+        d.__file__ = filename
+        with open(filename, 'rb') as config_file:
+            exec(compile(config_file.read(), filename, 'exec'), d.__dict__)
+        self.from_object(d)
+        return True
+
+    def from_object(self, obj):
+        """
+        Pull UPPERCASE attributes from `obj` and store in self.
+
+        :type obj: object
+        """
+        for key in dir(obj):
+            if key.isupper():
+                self[key] = getattr(obj, key)
+
+
+class DefaultConfig:
+    """
+    Configuration for use with RabbitMQ and SMTP running on localhost.
+    """
+    RABBITMQ_HOST = 'localhost'
+    SMTP_HOST = 'localhost'
+    QUEUE = 'emails'
+
+
+class Worker:
+    """
+    A task worker that sends emails.
+    """
+    def __init__(self, rabbitmq_host, smtp_host, queue):
+        """
+        :type rabbitmq_host: str
+        :type smtp_host: str
+        :param queue: The rabbitmq queue name.
+        :type queue: str
+        """
+        self.rabbitmq_host = rabbitmq_host
+        self.smtp_host = smtp_host
+        self.queue = queue
+
+    def ack_and_send_email(self, channel, method, properties, body):
+        """
+        Acknowledge message and attempt to send email. All exceptions are
+        caught and logged.
+        """
+        channel.basic_ack(delivery_tag = method.delivery_tag)
+        kwargs = deserializer(body)
+        kwargs['host'] = self.smtp_host
+        try:
+            bind_and_call(sendemail, kwargs)
+        except Exception as e:
+            if isinstance(e, KeyboardInterrupt):
+                raise
+            logger = logging.getLogger('emailworker.Worker')
+            logger.exception('%s', kwargs)
+
+    def start(self):
+        """
+        Start the message consumer.
+        """
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=self.rabbitmq_host)
         )
-    )
-    channel.close()
+        channel = connection.channel()
+        channel.queue_declare(queue=self.queue, durable=True)
+        channel.basic_consume(self.ack_and_send_email, queue=self.queue)
+        try:
+            channel.start_consuming()
+        except KeyboardInterrupt:
+            channel.close()
 
-def worker(channel, method, properties, body):
-    """
-    Deserialize message data and call `sendemail`.
-    """
-    logger = logging.getLogger('emailworker.worker')
-    bind_and_call(sendemail, json.loads(body))
-    channel.basic_ack(delivery_tag = method.delivery_tag)
-    logger.info('message acknowledged')
-
-def start():
-    """
-    Start the message consumer.
-    """
-    logger = logging.getLogger('emailworker.start')
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=config['emailworker']['rabbitmq_host'])
-    )
-    channel = connection.channel()
-    channel.queue_declare(queue=config['emailworker']['queue_name'], durable=True)
-
-    channel.basic_consume(worker, queue=config['emailworker']['queue_name'])
-    logger.info('starting worker')
-    try:
-        channel.start_consuming()
-    except KeyboardInterrupt:
-        channel.close()
-        logger.info('channel closed')
 
 def main():
     """
     Email Worker.
     """
     parser = argparse.ArgumentParser(prog='emailworker', description=main.__doc__)
-    parser.add_argument('--dryrun', action='store_true')
-    subparsers = parser.add_subparsers()
-
-    subparser = subparsers.add_parser('publish')
-    subparser.add_argument('fromaddr')
-    subparser.add_argument('toaddrs')
-    subparser.add_argument('--subject', default='')
-    subparser.add_argument('body')
-    subparser.set_defaults(func=publish)
-
-    subparser = subparsers.add_parser('start')
-    subparser.set_defaults(func=start)
-
     args = parser.parse_args()
 
-    if 'func' not in args:
-        parser.error('sub-command is required.')
+    config = Config()
+    config.from_object(DefaultConfig)
 
-    setup()
+    path = Path("./instance/config.py")
+    if path.exists():
+        config.from_pyfile(path)
 
-    config['emailworker']['dryrun'] = 'yes' if args.dryrun else 'no'
-    del args.dryrun
+    if 'LOGGING_CONF_DICT' in config:
+        logging.config.dictConfig(config['LOGGING_CONF_DICT'])
 
-    func = args.func
-    del args.func
-
-    bind_and_call(func, vars(args))
+    worker = Worker(config['RABBITMQ_HOST'], config['SMTP_HOST'], config['QUEUE'])
+    worker.start()
